af81c2439fca26064dda93a46d2b8109f7a04073
Blissysurya <150290655+Blissysurya@users.noreply.github.com>
2025-03-22 16:10:23 +0530

    as

---
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const LocationSearchPanel = () => {
+  return (
+    <div>
+        {/* this is just a sample data */}
+        <div className='flex my-4 gap-4 items-center justify-start'>
+            <h2 className='bg-[#eee] h-10 flex items-center justify-center w-10 rounded-full'>
+                <i className='ri-map-pin-fill'></i>
+            </h2>
+            <h4 className='font-medium'>NexEra Colony, Hardoi</h4>
+        </div>
+        <div className='flex my-4 gap-4 items-center justify-start'>
+            <h2 className='bg-[#eee] h-10 flex items-center justify-center w-10 rounded-full'>
+                <i className='ri-map-pin-fill'></i>
+            </h2>
+            <h4 className='font-medium'>NexEra Colony, Hardoi</h4>
+        </div>
+        <div className='flex my-4 gap-4 items-center justify-start'>
+            <h2 className='bg-[#eee] h-10 flex items-center justify-center w-10 rounded-full'>
+                <i className='ri-map-pin-fill'></i>
+            </h2>
+            <h4 className='font-medium'>NexEra Colony, Hardoi</h4>
+        </div>
+        <div className='flex my-4 gap-4 items-center justify-start'>
+            <h2 className='bg-[#eee] h-10 flex items-center justify-center w-10 rounded-full'>
+                <i className='ri-map-pin-fill'></i>
+            </h2>
+            <h4 className='font-medium'>NexEra Colony, Hardoi</h4>
+        </div>
+        <div className='flex my-4 gap-4 items-center justify-start'>
+            <h2 className='bg-[#eee] h-10 flex items-center justify-center w-10 rounded-full'>
+                <i className='ri-map-pin-fill'></i>
+            </h2>
+            <h4 className='font-medium'>NexEra Colony, Hardoi</h4>
+        </div>
+    </div>
+  )
+}
+
+export default LocationSearchPanel
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,40 @@
 import React from 'react'
+import {useGSAP}from '@gsap/react'
+import { useRef } from 'react'
+import { useState } from 'react'
+import gsap from 'gsap'
+import 'remixicon/fonts/remixicon.css'
+import LocationSearchPanel from '../components/LocationSearchPanel'
+
 
 const Home = () => {
 const [pickup, setPickup] = useState('')
 const [destination, setDestination] = useState('')
-con
+const [panelOpen, setPanelOpen] = useState(false)
+
+const panelRef=useRef(null)
+const panelCloseRef = useRef(null)
     const submitHandler=(e)=>{
         e.preventDefault()
         // console.log('Form submitted')
 
     }
 
+
+    useGSAP(function(){
+        console.log('Panel Ref:', panelRef.current);
+        if(panelOpen){
+            gsap.to(panelRef.current, {height:'70%',padding:24, duration:0.5})
+            gsap.to(panelCloseRef.current, {opacity:1, duration:0.5})
+        }else{
+            gsap.to(panelCloseRef.current, {opacity:0, duration:0.5})
+        
+            gsap.to(panelRef.current, {height:0,padding:0, duration:0.5})
+        }
+    },[panelOpen,panelCloseRef])
+
   return (
-    <div className='h-screen relative'>
+    <div className='h-screen relative overflow-hidden'>
         <img className='w-16 absolute left-5 top-5' src='https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png'></img>
         <div className='h-screen w-screen'>
             {/* Temporary image*/}
@@ -19,23 +42,41 @@ con
         </div>
         <div className=' flex flex-col justify-end h-screen absolute top-0 w-full'>
             <div className='h-[30%] p-5 bg-white relative'>
-            <h4 className='text-3xl font-semibold'>Find a trip</h4>
+               <h5 ref={panelCloseRef} onClick={()=>{setPanelOpen(false)}} className='absolute opacity-0 top-6 right-6 text-2xl'>
+                <i className='ri-arrow-down-wide-line'></i>
+               </h5>
+            <h4 className='text-2xl font-semibold'>Find a trip</h4>
             <form onSubmit={(e)=>{
                 submitHandler(e)
             }}>
                 <div className="line absolute h-16 w-1 top-[45%] left-10 bg-gray-900 rounded-full"></div>
                 <input
+                onClick={()=>{setPanelOpen(true)}}    
                 value={pickup}
                 onChange={(e)=>{setPickup(e.target.value)}}
-                className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full mt-5' type="text" placeholder='Add a pickup location' />
+                className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full mt-5 placeholder:text-base' type="text" placeholder='Add a pickup location' />
                 <input
+                onClick={()=>{setPanelOpen(true)}}
                 value={destination}
                 onChange={(e)=>{setDestination(e.target.value)}}
-                className='bg-[#eee] px-12  py-2 text-lg rounded-lg w-full mt-3' type="text" placeholder='Add a destination' />
+                className='bg-[#eee] px-12  py-2 text-lg rounded-lg w-full mt-3 placeholder:text-base' type="text" placeholder='Add a destination' />
             </form>
         </div>
-        <div className=' bg-red-500  h-0' />
-
+        <div ref={panelRef} className=' bg-white  h-0' >
+            <LocationSearchPanel/>
+        </div>
+        </div>
+        <div className='fixed z-10 bottom-0 p-5'>
+            <div className='flex items-center justify-between p-5'>
+               <img src='https://www.pngplay.com/wp-content/uploads/8/Uber-PNG-Photos.png'></img> 
+               <div>
+                <h4>UberGo <span><i className='ri-user-3-fill'></i>4</span></h4>
+                <h5>2 mins away</h5>
+                <p>Affordable, compact rides</p>
+               </div>
+               <h2>$10</h2>
+            </div>
+           
         </div>
     </div>
   )
